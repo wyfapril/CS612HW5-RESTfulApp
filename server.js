7fcb71e78c7c05e137be7edb132f6988afc1e146
wyfapril <yunfeiwang.april@gmail.com>
2017-12-08 14:59:05 -0500

    reformat image source route to show emoji pictures by category

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var path = require("path");
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  console.log(path.join(__dirname, "index.html"));
+  // console.log(path.join(__dirname, "index.html"));
 	res.sendFile(path.join(__dirname, "index.html"));
 })
 
 app.get('/allemojis', function (req, res) {
    fs.readFile( __dirname + "/" + "emojiData.json", 'utf8', function (err, data) {
-       console.log( data );
-      res.end( JSON.stringify(data));
+      // console.log( data );
+      res.end( JSON.stringify(data, null, 4));
    });
 })
 
@@ -21,19 +21,18 @@ app.get('/emoji/:id', function (req, res) {
    fs.readFile( __dirname + "/" + "emojiData.json", 'utf8', function (err, data) {
       var emojis = JSON.parse( data );
       var emo = emojis["emoji" + req.params.id] 
-      console.log( emo );
-      //res.end( JSON.stringify(emo));
-      console.log(path.join(__dirname)+'\\'+emo.location);
+      // console.log(path.join(__dirname)+'\\'+emo.location);
       res.set('Content-Type', 'image/png');
       res.sendFile(path.join(__dirname, emo.location));
    });
 })
+
 
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app listening at ", port)
 
 })
